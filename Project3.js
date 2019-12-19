906ca2bd89afe247d7004a5707e402dfe508c131
Gweezdo <z.trivium@gmail.com>
2019-12-20 10:01:38 +1100

    Updated Readme with project Brief Pjoject3 final solution + commented out code with different approach

--- Project3.js
+++ Project3.js
@@ -1,11 +1,11 @@
 function rot13(str) { // LBH QVQ VG!
 
-  let str1 = str.slice();
+  let str1 = str.slice();             // create a copy of the passed string
 
-  let str1Arr = str1.split('');
+  let str1Arr = str1.split('');       // split string into character array;
   console.log("str1Arr = " + str1Arr);
 
-  let alphabet = {
+  let alphabet = {                    // object holding all characters of the alphabet
     1: "A",
     2: "B",
     3: "C",
@@ -34,36 +34,67 @@ function rot13(str) { // LBH QVQ VG!
     26: "Z"
   }
 
-  let finalArray = [];
+  let finalArray = [];                // define empty array to push all shifted values to
 
 
+  function getKeyByValue(object, value){    // function that returns the object key when given the corresponding value
+    for (var key in object){
+      if(object[key] === value){
+        return key;
+      }
+    }
+  }
+
+  let alphaRegex = /^[A-Za-z]+$/;           // Regex to identify only alphabetic characters
   for(let i=0; i<str1Arr.length; i++){
-    for (var key in alphabet) { 
-      if (alphabet.hasOwnProperty(key)) { 
-          if (alphabet[key] === str1Arr[i]){
-            if(parseInt(key)<=13){
-              console.log("if Key Before: " + key);
-              let a = parseInt(key)+=13;
-              console.log("if Key After: " + a);
-              finalArray.push(alphabet[a]);
-            } else{
-              console.log("else Key Before: " + key);
-              let a = parseInt(key)-=13;
-              finalArray.push(alphabet[a]);
-              console.log("else Key After: " + a);
-              
-            }
-          } 
-        }
+    if(alphaRegex.test(str1Arr[i])){
+      let keyIntial = getKeyByValue(alphabet, str1Arr[i]);
+
+      if (parseInt(keyIntial) <= 13) {        // if key is less than 13, add 13 to get the shifted key value
+        let keyFinal = parseInt(keyIntial);
+        keyFinal += 13;
+        finalArray.push(alphabet[keyFinal]);
+      } else {
+        let keyFinal = parseInt(keyIntial);   // if key is greater than 13, subtract 13 to get the shifted key value.
+        keyFinal -= 13;
+        finalArray.push(alphabet[keyFinal]);
       }
 
+    }else{
+      finalArray.push(str1Arr[i]);
     }
+  }
+
+
+
+  // for(let i=0; i<str1Arr.length; i++){
+  //   for (var key in alphabet) { 
+  //     if (alphabet.hasOwnProperty(key)) { 
+  //         if (alphabet[key] === str1Arr[i]){
+  //           if(parseInt(key)<=13){
+  //             console.log("if Key Before: " + key);
+  //             let a = parseInt(key)+=13;
+  //             console.log("if Key After: " + a);
+  //             finalArray.push(alphabet[a]);
+  //           } else{
+  //             console.log("else Key Before: " + key);
+  //             let a = parseInt(key)-=13;
+  //             finalArray.push(alphabet[a]);
+  //             console.log("else Key After: " + a);
+              
+  //           }
+  //         } 
+  //       }
+  //     }
+
+  //   }
 
 
   console.log(finalArray)
-
-    return str;
+  let finalStr = finalArray.join("");
+  console.log(finalStr);
+  return finalStr;
 }
   
   // Change the inputs below to test
-  rot13("SERR PBQR PNZC");
+  rot13("SERR! PBQR PNZC");
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # FCC_JS_Project3
 Free Code Camp JavaScript Project 3 - Caesars Cipher
 
+JavaScript Algorithms and Data Structures Projects: Caesars Cipher
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
 Proposed steps to solve problem:
 1. try to write a pure function 
 2. make a copy of the passed string
